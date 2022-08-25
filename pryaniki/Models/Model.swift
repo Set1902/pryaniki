4d795a2fcedc4ad5383023b3539975a94c1fc190
Sergei Kovalev <skovalev1902@gmail.com>
2022-08-26 02:10:22 +0300

    r

---
+++ pryaniki/Models/Model.swift
@@ -0,0 +1,43 @@
+//
+//  Model.swift
+//  pryaniki
+//
+//  Created by Sergei Kovalev on 26.08.2022.
+//
+
+import Foundation
+import Combine
+
+
+// MARK: - Cart
+struct Cart: Decodable {
+    var data: [Datum]?
+    var view: [String]?
+}
+
+
+// MARK: - Datum
+struct Datum: Decodable {
+    var name: String?
+    var data: DataClass?
+}
+
+// MARK: - DataClass
+struct DataClass: Decodable {
+    var text: String?
+    var url: String?
+    var selectedID: Int?
+    var variants: [Variant]?
+
+    enum CodingKeys: String, CodingKey {
+        case text, url
+        case selectedID = "selectedId"
+        case variants
+    }
+}
+
+// MARK: - Variant
+struct Variant: Decodable {
+    var id: Int?
+    var text: String?
+}
---
+++ pryaniki/Service/Service.swift
@@ -0,0 +1,9 @@
+//
+//  Service.swift
+//  pryaniki
+//
+//  Created by Sergei Kovalev on 26.08.2022.
+//
+
+import Foundation
+import Combine
